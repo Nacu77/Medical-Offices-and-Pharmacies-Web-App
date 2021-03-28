5dfc9e847aab9c3dceb5e3ca0a2212820ca7071f
Nacu Florin <florin.nacu77@gmail.com>
2021-03-28 17:22:43 +0300

    first commit

---
+++ src/main/java/com/nacu/medicaloffices/MedicalofficesApplication.java
@@ -0,0 +1,13 @@
+package com.nacu.medicaloffices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalofficesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedicalofficesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nacu/medicaloffices/MedicalofficesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nacu.medicaloffices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalofficesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
